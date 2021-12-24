71d647077382d5f598e45644e7be17cc04cee048
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2021-12-24 16:43:54 +0700

    feat: navbar(include responsive)

--- src/pages/Users/ReportPage.jsx
+++ src/pages/Users/ReportPage.jsx
@@ -1,45 +1,54 @@
-import { Box, Button, Center, Flex, FormControl, FormLabel, Heading, Input, Select, VStack } from '@chakra-ui/react'
+import { Box, Button, Center, Container, Flex, FormControl, FormLabel, Heading, Input, Select, VStack } from '@chakra-ui/react'
 import React from 'react'
 import Footer from '../../components/Footer'
+import Navbar from '../../components/Navbar'
+import NavBotom from '../../components/NavBotom'
 
 const ReportPage = () =>
 {
     return (
-        <Flex
-            p={ 50 }
-            w="full"
-            alignItems="center"
-            justifyContent="center"
-            direction={ 'column' }
-        >
-            <Box
-                mx="auto"
-                rounded="lg"
-                shadow="md"
-                w={ [ '340', '530px' ] }
-                maxW="2xl"
-            >
-                <Box p={ 6 }>
-                    <Center>
-                        <Heading>Laporkan</Heading>
-                    </Center>
-                    <VStack spacing={ 5 } mt={ 10 }>
-                        <FormControl>
-                            <FormLabel htmlFor='masalah'>Tulis alasan anda ingin melaporkan postingan !</FormLabel>
-                            <Input type='text' id='masalah' />
-                        </FormControl>
-                        <FormControl>
-                            <FormLabel htmlFor='daftarmasalah'>Pilih Masalah</FormLabel>
-                            <Select id='daftarmasalah' placeholder='Pilih Masalah'>
-                                <option>United Arab Emirates</option>
-                                <option>Nigeria</option>
-                            </Select>
-                        </FormControl>
-                        <Button variant={ 'solid' } colorScheme={ 'purple' } w={ 'full' }>Submit</Button>
-                    </VStack>
-                </Box>
-            </Box>
-            <Footer />
+        <Flex direction={'column'}>
+            <Navbar />
+            <NavBotom />
+            <Container centerContent h={ '100vh' }>
+                <Flex
+                    p={ 50 }
+                    w="full"
+                    mt={20}
+                    alignItems="center"
+                    justifyContent="center"
+                    direction={ 'column' }
+                >
+                    <Box
+                        mx="auto"
+                        rounded="lg"
+                        shadow="md"
+                        w={ [ '340', '530px' ] }
+                        maxW="2xl"
+                    >
+                        <Box p={ 6 }>
+                            <Center>
+                                <Heading>Laporkan</Heading>
+                            </Center>
+                            <VStack spacing={ 5 } mt={ 10 }>
+                                <FormControl>
+                                    <FormLabel htmlFor='masalah'>Tulis alasan anda ingin melaporkan postingan !</FormLabel>
+                                    <Input type='text' id='masalah' />
+                                </FormControl>
+                                <FormControl>
+                                    <FormLabel htmlFor='daftarmasalah'>Pilih Masalah</FormLabel>
+                                    <Select id='daftarmasalah' placeholder='Pilih Masalah'>
+                                        <option>United Arab Emirates</option>
+                                        <option>Nigeria</option>
+                                    </Select>
+                                </FormControl>
+                                <Button variant={ 'solid' } colorScheme={ 'purple' } w={ 'full' }>Submit</Button>
+                            </VStack>
+                        </Box>
+                    </Box>
+                </Flex>
+                <Footer />
+            </Container>
         </Flex>
     )
 }
