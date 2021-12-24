d7be50b9e8807f7aa3ad6ca8a8952e6e03d8187c
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2021-12-24 16:43:32 +0700

    feat: navbar(include responsive)

--- src/pages/Users/ProfileFriends.jsx
+++ src/pages/Users/ProfileFriends.jsx
@@ -10,77 +10,83 @@ import ProfilePict from '../../components/ProfilePict';
 import cover from "../../assets/img/629527.jpg";
 import Profile from "../../assets/img/Rectangle 42.png";
 import Footer from '../../components/Footer';
+import Navbar from '../../components/Navbar';
+import NavBotom from '../../components/NavBotom';
 
 
 const ProfileFriends = () =>
 {
     const [ Mobile ] = useMediaQuery( "(min-width: 500px)" );
     return (
-        <Container centerContent h={ '100vh' }>
-            <CoverImage cover={ cover } />
-            <VStack>
-                { Mobile ? (
-                    <Flex direction={ "column" }>
-                        <HStack spacing={ { md: "3em", lg: "10em", xl: "13em" } }>
-                            <ProfilePict Profile={ Profile } Top={ 60 } />
-                            <NameUsers
-                                username={ "Spiderman Ganteng" }
-                                tagName={ "Newbiers" }
-                                bio={ "Kang Ngarang" }
-                            />
-                            <HStack>
-                                <ButtonProfiles messages={ "Follow" } />
-                                <ButtonProfiles messages={ "Messages" } />
-                            </HStack>
-                        </HStack>
-                        <VStack>
-                            <Flex mt={ { md: 20, lg: 8 } } direction={ "column" }>
-                                <HStack spacing={ { md: "2em", lg: "5em" } }>
-                                    <Achievement post={ 3 } thread={ 5 } />
-                                    <HStack>
-                                        <Box width={ { md: "20em", lg: "30em" } }>
-                                            <Konten kiri={ "Post" } kanan={ "Thread" } />
-                                        </Box>
-                                    </HStack>
-                                    <Badges iconbadges={ "Icon" } title={ "Suka cerita" } />
-                                </HStack>
-                            </Flex>
-                        </VStack>
-                    </Flex>
-                ) : (
-                    <VStack>
-                        <Center>
-                            <ProfilePict Profile={ Profile } Mb={ 30 } />
-                        </Center>
-                        <Flex
-                            direction={ "column" }
-                            justifyContent={ "center" }
-                            top={ 20 }
-                            position={ "relative" }>
-                            <Box align={ "center" } mb={ 5 }>
+        <Flex direction={"column"}>
+            <Navbar />
+            <NavBotom />
+            <Container centerContent h={ '100vh' }>
+                <CoverImage cover={ cover } />
+                <VStack>
+                    { Mobile ? (
+                        <Flex direction={ "column" }>
+                            <HStack spacing={ { md: "3em", lg: "10em", xl: "13em" } }>
+                                <ProfilePict Profile={ Profile } Top={ 60 } />
                                 <NameUsers
-                                    username={ "Spiderman" }
+                                    username={ "Spiderman Ganteng" }
                                     tagName={ "Newbiers" }
                                     bio={ "Kang Ngarang" }
                                 />
-                            </Box>
-                            <Center>
-                                <HStack spacing={ 2 }>
+                                <HStack>
                                     <ButtonProfiles messages={ "Follow" } />
                                     <ButtonProfiles messages={ "Messages" } />
                                 </HStack>
-                            </Center>
-                            <Achievement post={ 3 } thread={ 5 } />
-                            <Badges iconbadges={ "Icon" } title={ "Suka cerita" } />
-                            <Center mt={ 5 }>
-                                <Konten kiri={ "Post" } kanan={ "Thread" } />
-                            </Center>
+                            </HStack>
+                            <VStack>
+                                <Flex mt={ { md: 20, lg: 8 } } direction={ "column" }>
+                                    <HStack spacing={ { md: "2em", lg: "5em" } }>
+                                        <Achievement post={ 3 } thread={ 5 } />
+                                        <HStack>
+                                            <Box width={ { md: "20em", lg: "30em" } }>
+                                                <Konten kiri={ "Post" } kanan={ "Thread" } />
+                                            </Box>
+                                        </HStack>
+                                        <Badges iconbadges={ "Icon" } title={ "Suka cerita" } />
+                                    </HStack>
+                                </Flex>
+                            </VStack>
                         </Flex>
-                    </VStack>
-                ) }
-            </VStack>
-            <Footer />
-        </Container>
+                    ) : (
+                        <VStack>
+                            <Center>
+                                <ProfilePict Profile={ Profile } Mb={ 30 } />
+                            </Center>
+                            <Flex
+                                direction={ "column" }
+                                justifyContent={ "center" }
+                                top={ 20 }
+                                position={ "relative" }>
+                                <Box align={ "center" } mb={ 5 }>
+                                    <NameUsers
+                                        username={ "Spiderman" }
+                                        tagName={ "Newbiers" }
+                                        bio={ "Kang Ngarang" }
+                                    />
+                                </Box>
+                                <Center>
+                                    <HStack spacing={ 2 }>
+                                        <ButtonProfiles messages={ "Follow" } />
+                                        <ButtonProfiles messages={ "Messages" } />
+                                    </HStack>
+                                </Center>
+                                <Achievement post={ 3 } thread={ 5 } />
+                                <Badges iconbadges={ "Icon" } title={ "Suka cerita" } />
+                                <Center mt={ 5 }>
+                                    <Konten kiri={ "Post" } kanan={ "Thread" } />
+                                </Center>
+                            </Flex>
+                        </VStack>
+                    ) }
+                </VStack>
+                <Footer />
+            </Container>
+        </Flex>
     );
 }
 
