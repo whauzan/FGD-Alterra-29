c1a07c60ae354bd593e3677dc264e2ac4b220077
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2021-12-28 09:20:58 +0700

    feat: route to create-thread

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import HomePage from "./pages/Users/HomePage.jsx";
 import ProfileFriends from "./pages/Users/ProfileFriends.jsx";
 import ReportPage from "./pages/Users/ReportPage"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CreateThread from "./pages/Users/CreateThread.jsx";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='/user/id' element={<ProfileFriends />} />
         <Route path='/report/id' element={<ReportPage />} />
+        <Route path='/create-thread' element={<CreateThread />} />
       </Routes>
     </Router>
   );
