5a9f8395b1b5767a59b42f6846c4576028e1e70f
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2022-01-10 10:32:27 +0700

    feat: route

--- src/App.js
+++ src/App.js
@@ -3,17 +3,30 @@ import ProfileFriends from "./pages/Users/ProfileFriends.jsx";
 import ReportPage from "./pages/Users/ReportPage"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CreateThread from "./pages/Users/CreateThread.jsx";
+import { persistor, store } from "./Redux/store.js";
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import UsersProfile from "./pages/Users/UsersProfile.jsx";
+import SettingUsers from "./pages/Users/SettingUsers.jsx";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/user/id' element={<ProfileFriends />} />
-        <Route path='/report/id' element={<ReportPage />} />
-        <Route path='/create-thread' element={<CreateThread />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/recommendation' element={<HomePage />} />
+            <Route path='/hot-thread' element={<HomePage />} />
+            <Route path='/user/id' element={<ProfileFriends />} />
+            <Route path='/report/id' element={<ReportPage />} />
+            <Route path='/create-thread' element={<CreateThread />} />
+            <Route path='/my-profile' element={<UsersProfile />} />
+            <Route path='/my-profile/setting' element={<SettingUsers />} />
+          </Routes>
+        </Router>
+      </PersistGate>
+    </Provider>
   );
 }
 
