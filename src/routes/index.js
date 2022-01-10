566cfaa24580b9aaeebf2dad2756731a14a5c4cf
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2022-01-10 17:18:08 +0700

    fix: route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import { persistor, store } from "../Redux/store.js";
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import Overview from "../pages/Admin/Overview";
 import Report from "../pages/Admin/Report";
 import SettingUsers from "../pages/Admin/SettingUsers";
@@ -9,21 +12,33 @@ import HomePage from "../pages/Users/HomePage";
 import ProfileFriends from "../pages/Users/ProfileFriends";
 import ReportPage from "../pages/Users/ReportPage";
 import Post from "../pages/Admin/Post";
+import CreateThread from "../pages/Users/CreateThread.jsx";
+import UsersProfile from "../pages/Users/UsersProfile.jsx";
+import SettingUser from "../pages/Users/SettingUser.jsx";
 const Routers = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/user/id' element={<ProfileFriends />} />
-        <Route path='/report/id' element={<ReportPage />} />
-        <Route path='/admin/overview' element={<Overview />} />
-        <Route path='/admin/users' element={<Users />} />
-        <Route path='/admin/post' element={<Post />} />
-        <Route path='/admin/thread' element={<Thread />} />
-        <Route path='/admin/report' element={<Report />} />
-        <Route path='/admin/setting' element={<SettingUsers />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/recommendation' element={<HomePage />} />
+            <Route path='/hot-thread' element={<HomePage />} />
+            <Route path='/user/id' element={<ProfileFriends />} />
+            <Route path='/report/id' element={<ReportPage />} />
+            <Route path='/create-thread' element={<CreateThread />} />
+            <Route path='/my-profile' element={<UsersProfile />} />
+            <Route path='/my-profile/setting' element={<SettingUser />} />
+            <Route path='/admin/overview' element={<Overview />} />
+            <Route path='/admin/users' element={<Users />} />
+            <Route path='/admin/post' element={<Post />} />
+            <Route path='/admin/thread' element={<Thread />} />
+            <Route path='/admin/report' element={<Report />} />
+            <Route path='/admin/setting' element={<SettingUsers />} />
+          </Routes>
+        </Router>
+      </PersistGate>
+    </Provider>
   );
 };
 
